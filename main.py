03835e87f03b980b468a0ab87ab172be31e1c701
Keshav-63 <keshavdv241@gmail.com>
2026-05-26 01:05:47 +0530

    basic CRUD api snippet

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from gettext import find
+
 from fastapi import FastAPI, Body, Response, status, HTTPException
 from pydantic import BaseModel
 from typing import Optional
@@ -22,13 +24,20 @@ def find_post_by_id(id):
     for p in my_posts:
         if p['id'] == id:
             return p
+        
+        
+def find_index_of_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
+        
 
 @app.get("/")
 def root():
     return {"message" : "Hello World"}
 
 
-@app.post("/new_posts")
+@app.post("/new_posts", status_code=status.HTTP_201_CREATED)
 def new_posts(new_post: Post):
     post_dict = new_post.model_dump() # use model_dump bcz dict is deprecated
     post_dict['id'] = randrange(0, 100)
@@ -51,9 +60,39 @@ def get_post_by_id(id : int, response: Response):
 @app.get("/posts")
 def get_posts():
     return {"data": my_posts}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
     
+    index = find_index_of_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail = f"post with id: {id} was not found")
+        
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+    
+
+@app.put("/posts/{id}", status_code=status.HTTP_200_OK)
+def update_post(id: int, post: Post):
+    
+    index = find_index_of_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail = f"post with id: {id} was not found")
+    
+    post_dict = post.model_dump()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+    return {"data": f"updated post {my_posts[index]}"}
+        
+    
+
 
 @app.post("/createposts")
 def create_posts(payload: dict = Body(...)):
     print(payload)
     return {"new_post": f"title: {payload['title']}, content: {payload['content']}"}
+
+
